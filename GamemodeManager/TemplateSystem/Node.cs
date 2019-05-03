d3c3aadc7fa46a857853ba8dc1cdf90b465aaffc
Lord of Khaos <liam@billandmary.com>
2019-05-02 21:57:56 -0400

    Made an oopsie with the commits, see previous

---
+++ GamemodeManager/TemplateSystem/Node.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GamemodeManager.TemplateSystem
+{
+	public class Node
+	{
+		public Node(string key, bool isNested)
+		{
+			this.Key = key;
+			this.IsNested = isNested;
+		}
+
+		public Node(string key)
+		{
+			this.Key = key;
+		}
+
+		public string Key { get; set; }
+
+		public string Value { get; set; }
+
+		public List<Tuple<string, string>> Values { get; set; }
+
+		public bool IsNested { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			return obj is Node node &&
+				   this.Key == node.Key &&
+				   this.Value == node.Value &&
+				   EqualityComparer<List<Tuple<string, string>>>.Default.Equals(this.Values, node.Values) &&
+				   this.IsNested == node.IsNested;
+		}
+
+		public override int GetHashCode()
+		{
+			int hashCode = -1553581789;
+			hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(this.Key);
+
+			if (this.IsNested)
+				hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(this.Value);
+			else
+				hashCode = hashCode * -1521134295 + EqualityComparer<List<Tuple<string, string>>>.Default.GetHashCode(this.Values);
+
+			hashCode = hashCode * -1521134295 + this.IsNested.GetHashCode();
+			return hashCode;
+		}
+	}
+}
---
+++ GamemodeManager/TemplateSystem/Template.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GamemodeManager.TemplateSystem
+{
+	public class Template
+	{
+		public Template()
+		{
+			this.AssignProperties();
+		}
+
+		public Template(string pluginId, string[] lines)
+		{
+			this.PluginId = pluginId;
+			this.Lines = lines;
+
+			this.AssignProperties();
+		}
+
+		public string PluginId { get; set; }
+
+		public string[] Lines { get; set; }
+
+		public string TemplateName { get; private set; }
+
+		public string Name { get; set; }
+
+		public int Rounds { get; set; }
+
+		public string SpawnQueue { get; set; }
+
+		public string Description { get; set; }
+
+		public List<Node> Nodes { get; set; }
+
+		
+
+		private void AssignProperties()
+		{
+			foreach (Node node in this.Nodes)
+			{
+				switch (node.Key.ToUpper())
+				{
+					case "TEMPLATENAME":
+						this.TemplateName = node.Value;
+						break;
+					case "NAME":
+						this.Name = node.Value;
+						break;
+					case "ROUNDS":
+						this.Rounds = int.TryParse(node.Value, out int r) ? r : 1;
+						break;
+					case "SPAWNQUEUE":
+						this.SpawnQueue = int.TryParse(string.Join(string.Empty, node.Value.Split().Range(node.Value.Length - 2)), out int f)
+											  ? node.Value
+											  : "40143140314414041340";
+						break;
+					case "DESCRIPTION":
+						this.Description = node.Value;
+						break;
+					default:
+						break;
+				}
+			}
+		}
+
+		public override bool Equals(object obj)
+		{
+			return base.Equals(obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+	}
+}
